d12c56aac1a27a6e932441ca62640426b3d7297f
Eddie Lopez <ed98lopez@gmail.com>
2020-11-07 09:39:01 -0800

    finished the header.js running into trouble with the tabls tho. we will see how it goes

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,35 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+
+
+    // create div, set class name, append to header-container
+    const header = document.createElement('div')
+    header.className = "header"
+    
+    const headerContainer = document.querySelector('.header-container')
+    headerContainer.appendChild(header)
+
+    //span date
+
+    const headerDate = document.createElement('span')
+    headerDate.className = 'date'
+    headerDate.textContent = 'MARCH 28,2020'
+    header.appendChild(headerDate)
+
+    //h1
+    const headerH1 = document.createElement('h1')
+    headerH1.textContent = 'Lambda Times'
+    header.appendChild(headerH1)
+
+    //span temp
+    const temp = document.createElement('span')
+    temp.className = 'temp'
+    temp.textContent = '98°'    
+    header.appendChild(temp)
+
+    return header
+}
+
+Header()
